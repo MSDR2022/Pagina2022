8942aa81c6428c4d4ded000ba7e215d8218b4cab
Diseng <mjguette21@gmail.com>
2022-09-14 15:35:01 -0500

    Revert "mapa funcional si data"
    This reverts commit 6aab52048d77369c539423b455fba3bab0d815c5.

--- mapa.js
+++ mapa.js
@@ -1,4 +1,3 @@
-//Contenedor del mapa
 var container = L.DomUtil.get("map");
 
 if(container != null){
@@ -11,19 +10,15 @@ map.invalidateSize(); // Si hay un mapa, lo elimina para recrearlo y que se pued
 
 var L = window.L;
 
-//Declaro el mapa
-var map = L.map("map"). 
+var map = L.map("map").
 setView([11.0249, -74.8551],
         17);
 
-//Atributos del mapa
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 }).addTo(map);
 
-//Control del mapa
 L.control.scale().addTo(map);
 
-//Marcador
 L.marker([11.0249, -74.8551],{draggable: true}).addTo(map);
