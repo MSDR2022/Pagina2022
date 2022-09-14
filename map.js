549a31549501b3d59552ceee303fd2fe5da0d9a3
Diseng <mjguette21@gmail.com>
2022-09-14 01:24:28 -0500

    Map.js

---
+++ map.js
@@ -0,0 +1,27 @@
+      //Mapa
+      var container = L.DomUtil.get('map')
+
+      //Mapas if
+      if (container != null) {
+        container._leaflet_id = null;
+      }
+
+
+      if (map) {
+        map.invalidateSize(); // Si hay un mapa, lo elimina para recrearlo y que se pueda cambiar actualmente la posición ##
+      }
+
+      //Declaracion del mapa
+      var map = L.map('map');
+
+      map = L.map('map').setView([data.Latitud, data.Longitud], 13);
+
+      //Estilo del mapa
+      var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+function mark  (){
+      //Marcador
+      var marker = L.marker([data.Latitud, data.Longitud]).addTo(map)
+        .bindPopup('<b>' + data.Fecha + '</b>' + '<br />' + data.Latitud + ' ' + data.Longitud).openPopup();
+    }
+mark()
+setInterval(mark, 4900)
