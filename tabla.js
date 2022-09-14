6e9163ac102c4ca1e1ee319898a78b92a9e0155d
Diseng <mjguette21@gmail.com>
2022-09-14 14:41:21 -0500

    tabla 2

--- tabla.js
+++ tabla.js
@@ -1,13 +1,12 @@
-  function table() {
-    const xhttp = new XMLHttpRequest();  //creando el objeto para trabajar
-
-    xhttp.open("GET", "datadb.php");  // documento que estamos llamando
-    xhttp.send(); //Envio data
-  }
+ // function table() {
+   // const xhttp = new XMLHttpRequest();  //creando el objeto para trabajar
+    //xhttp.open("GET", "datadb.php");  // documento que estamos llamando
+    //xhttp.send(); //Envio data
+ // }
 
 
   //Funcion para el delay
-  setInterval(
-    function () {   //creamos un intervalo para realizar el mismo request del archivo que genera la conexión con la base de datos
-      table();
-    }, 5000); // Cada 5 segundos
+  //setInterval(
+   // function () {   //creamos un intervalo para realizar el mismo request del archivo que genera la conexión con la base de datos
+     // table();
+    //}, 5000); // Cada 5 segundos
