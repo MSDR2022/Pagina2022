c74ba8e84b4db12f6b2c0870530f53e4e1edd800
Diseng <mjguette21@gmail.com>
2022-09-14 15:00:15 -0500

    Mapa func

--- mapa.js
+++ mapa.js
@@ -1,12 +1,3 @@
-var container = L.DomUtil.get("map");
-
-if (container != null) {
-    container._leaflet_id = null;
-}
-
-var L = window.L;
-
-
 const xhttp = new XMLHttpRequest();  //creando el objeto para trabajar
 xhttp.onload = function () {
     var data = this.responseText
@@ -15,20 +6,33 @@ xhttp.onload = function () {
     document.getElementById("lng").innerHTML = data.Longitud
     document.getElementById("date").innerHTML = data.Fecha
 
+    var container = L.DomUtil.get("map");
+
+    if (container != null) {
+        container._leaflet_id = null;
+    }
+
+    if (map) {
+        map.invalidateSize(); // Si hay un mapa, lo elimina para recrearlo y que se pueda cambiar actualmente la posición ##
+    }
+
+    var L = window.L;
+
     var map = L.map("map").
         setView([data.Latitud, data.Longitud],
-
             13);
+
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+        maxZoom: 18
+    }).addTo(map);
+
+    L.control.scale().addTo(map);
+
+    L.marker([data.Latitud, data.Longitud], { draggable: true }).addTo(map);
+
+
 }
 
 xhttp.open("GET", "datadb.php");  // documento que estamos llamando
 xhttp.send();
-
-
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
-}).addTo(map);
-
-L.control.scale().addTo(map);
-
