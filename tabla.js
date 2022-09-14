a58f5d30b4630346aad286ffb8e891824d3e20e8
Diseng <mjguette21@gmail.com>
2022-09-14 14:15:06 -0500

    prueba mapa separado

---
+++ tabla.js
@@ -0,0 +1,21 @@
+  function table() {
+    const xhttp = new XMLHttpRequest();  //creando el objeto para trabajar
+    xhttp.onload = function () {
+      var data = this.responseText
+      data = JSON.parse(data);
+
+      document.getElementById("lat").innerHTML = data.Latitud
+      document.getElementById("lng").innerHTML = data.Longitud
+      document.getElementById("date").innerHTML = data.Fecha
+    }
+
+    xhttp.open("GET", "datadb.php");  // documento que estamos llamando
+    xhttp.send(); //Envio data
+  }
+
+
+  //Funcion para el delay
+  setInterval(
+    function () {   //creamos un intervalo para realizar el mismo request del archivo que genera la conexión con la base de datos
+      table();
+    }, 5000); // Cada 5 segundos
