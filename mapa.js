e3632e33bf80e4ccd3a6a5b0db66d4b33527f788
Diseng <mjguette21@gmail.com>
2022-09-14 14:27:19 -0500

    Mapa web

--- mapa.js
+++ mapa.js
@@ -1,25 +1,12 @@
-//Mapa
-var container = L.DomUtil.get('map')
+var map = L.map('map').
+    setView([41.66, -4.72],
+        15);
 
-//Mapas if
-if (container != null) {
-  container._leaflet_id = null;
-}
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+}).addTo(map);
 
+L.control.scale().addTo(map);
 
-if (map) {
-  map.invalidateSize(); // Si hay un mapa, lo elimina para recrearlo y que se pueda cambiar actualmente la posición ##
-}
-
-//Declaracion del mapa
-var map = L.map('map');
-
-map = L.map('map').setView([data.Latitud, data.Longitud], 13);
-
-//Estilo del mapa
-var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-
-
-//Marcador
-var marker = L.marker([data.Latitud, data.Longitud]).addTo(map)
-  .bindPopup('<b>' + data.Fecha + '</b>' + '<br />' + data.Latitud + ' ' + data.Longitud).openPopup();
+L.marker([41.66, -4.71],{draggable: true}).addTo(map);
