c7dcf00693a65137f00628b3b25e2f3194067534
losadas <losadas@uninorte.edu.co>
2022-09-14 15:57:19 -0500

    calen

---
+++ factu.js
@@ -0,0 +1,13 @@
+function actual() {
+
+    var fecha = new Date();
+    var anio = fecha.getFullYear();
+    var dia = fecha.getDate();
+    var _mes = fecha.getMonth();
+    _mes = _mes + 1;
+    if (_mes < 10)
+    { var mes = "0" + _mes;}
+    else
+    { var mes = _mes.toString;}
+    document.getElementById("fechaReserva").min = anio+'-'+mes+'-'+dia;
+}
