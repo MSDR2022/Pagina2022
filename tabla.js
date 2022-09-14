dac46ae5f47d7daef4efc4c633f60dc731de4570
Diseng <mjguette21@gmail.com>
2022-09-14 14:21:24 -0500

    tabla

--- tabla.js
+++ tabla.js
@@ -1,13 +1,5 @@
   function table() {
     const xhttp = new XMLHttpRequest();  //creando el objeto para trabajar
-    xhttp.onload = function () {
-      var data = this.responseText
-      data = JSON.parse(data);
-
-      document.getElementById("lat").innerHTML = data.Latitud
-      document.getElementById("lng").innerHTML = data.Longitud
-      document.getElementById("date").innerHTML = data.Fecha
-    }
 
     xhttp.open("GET", "datadb.php");  // documento que estamos llamando
     xhttp.send(); //Envio data
