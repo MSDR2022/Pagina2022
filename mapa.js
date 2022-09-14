e1b70d43a528384d07bc7b9e8dd166d3f5c8218d
Diseng <mjguette21@gmail.com>
2022-09-14 15:10:54 -0500

    Ajuste zoom 2

--- mapa.js
+++ mapa.js
@@ -12,11 +12,11 @@ var L = window.L;
 
 var map = L.map("map").
 setView([11.0249, -74.8551],
-        16);
+        17);
 
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 17
+    maxZoom: 18
 }).addTo(map);
 
 L.control.scale().addTo(map);
