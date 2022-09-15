522a998547e25f64c645fafc44eaf6da7cb621c4
Linerosky <daniellinerohd@gmail.com>
2022-09-14 22:30:34 -0500

    maybe?

--- mapa.js
+++ mapa.js
@@ -22,4 +22,3 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.control.scale().addTo(map);
 
 L.marker(["data.Latitud", "data.Longitud"]).addTo(map);
-console.log("On the map: " + data.Latitud);
