65b5b9eb8fc66441ef042d8291b22c417d91cad7
Diseng <mjguette21@gmail.com>
2022-09-22 02:05:19 -0500

    prueba poli

--- historicos.php
+++ historicos.php
@@ -1,7 +1,7 @@
 <?php
 include 'varia.php';
 $conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
-$rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '2022-09-21 04:06:02' AND '2022-09-21 12:30:40'"); // genera el query a SQL
+$rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '2022-09-21 04:06:02' AND '2022-09-23 12:30:40'"); // genera el query a SQL
 $fila = mysqli_fetch_assoc($rows);
 echo json_encode($fila);
 ?>
