6f584a651f2429ef69beea771a65a6bf7cff7825
Linerosky <daniellinerohd@gmail.com>
2022-09-22 08:01:04 -0500

    array[]

--- historicos.php
+++ historicos.php
@@ -1,12 +1,7 @@
 <?php
 include 'varia.php';
 $conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
-$rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '2022-09-14 04:06:02' AND '2022-09-23 12:30:40'") or die("database error:". mysqli_error($conn)); // genera el query a SQL
-while($row = mysqli_fetch_array($rows)) {
-    $fila[] = array(
-    'Latitud' => $row['Latitud'],
-    'Longitud' => $row['Longitud'],
-    'Fecha' => $row['Fecha']
-    );
+$rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '2022-09-14 04:06:02' AND '2022-09-23 12:30:40'"); // genera el query a SQL
+$fila[] = mysqli_fetch_array($rows);
 echo json_encode($fila);
 ?>
