10c8dbc0e2cc9e19097217329c2989ea5bbeee25
Linerosky <daniellinerohd@gmail.com>
2022-09-22 07:52:32 -0500

    historicos not working . . . unknown

--- historicos.php
+++ historicos.php
@@ -1,7 +1,7 @@
 <?php
 include 'varia.php';
 $conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
-$rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '2022-09-21 04:06:02' AND '2022-09-23 12:30:40'"); // genera el query a SQL
+$rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '2022-09-14 04:06:02' AND '2022-09-23 12:30:40'") or die("database error:". mysqli_error($conn));; // genera el query a SQL
 while($row = mysqli_fetch_array($rows)) {
     $fila[] = array(
     'Latitud' => $row['Latitud'],
