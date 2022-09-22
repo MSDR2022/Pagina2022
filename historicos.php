7b5f7d06a17a3823f0e4f1121b2d02c91c4c7dd1
Linerosky <daniellinerohd@gmail.com>
2022-09-22 17:50:44 -0500

    jQuery

--- historicos.php
+++ historicos.php
@@ -1,5 +1,8 @@
 <?php
-
+$var_1 = $_POST["var_1"];
+$var_2 = $_POST["var_2"];
+$var_3 = $_POST["var_3"];
+$var_4 = $_POST["var_4"];
 include 'varia.php';
 $conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
 $rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '2022-09-14 04:06:02' AND '2022-09-23 12:30:40'"); // genera el query a SQL
