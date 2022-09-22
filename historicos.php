6a96458099a6a6fe71ac43901fc71788fd2e4500
Diseng <mjguette21@gmail.com>
2022-09-22 12:19:24 -0500

    echo

--- historicos.php
+++ historicos.php
@@ -4,4 +4,7 @@ $conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conex
 $rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '2022-09-14 04:06:02' AND '2022-09-23 12:30:40'"); // genera el query a SQL
 $fila = mysqli_fetch_all($rows, MYSQLI_ASSOC);
 echo json_encode($fila);
+
+$var_1 = $_POST["var_1"];
+echo json_encode($var_1);
 ?>
