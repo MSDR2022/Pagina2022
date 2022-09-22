a7e4a2b6c6874abf834da982d60d74480d7e4f20
fuentesrr <rrfuentes@uninorte.edu.co>
2022-09-22 10:37:40 -0500

    prueba f

--- historicos.php
+++ historicos.php
@@ -2,6 +2,6 @@
 include 'varia.php';
 $conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $Bname);  // Establece conexi�n
 $rows = mysqli_query($conn, "SELECT * FROM bdesign.Basedatos WHERE Fecha BETWEEN '2022-09-21 04:06:02' AND '2022-09-23 12:30:40'"); // genera el query a SQL
-$fila = mysqli_fetch_assoc($rows, MYSQLI_ASSOC);
+$fila = mysqli_fetch_assoc($rows);
 echo json_encode($fila);
 ?>
