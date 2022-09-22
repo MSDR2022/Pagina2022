05f1970356acc62df71f8a6e2372e507a0e69b7f
fuentesrr <rrfuentes@uninorte.edu.co>
2022-09-22 09:30:44 -0500

    Actualizado

--- historicos.php
+++ historicos.php
@@ -1,7 +1,7 @@
 <?php
 include 'varia.php';
-$conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
-$rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '2022-09-21 04:06:02' AND '2022-09-23 12:30:40'"); // genera el query a SQL
+$conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $Bname);  // Establece conexi�n
+$rows = mysqli_query($conn, "SELECT * FROM bdesign.Basedatos WHERE Fecha BETWEEN '2022-09-21 04:06:02' AND '2022-09-23 12:30:40'"); // genera el query a SQL
 $fila = mysqli_fetch_assoc($rows);
 echo json_encode($fila);
 ?>
