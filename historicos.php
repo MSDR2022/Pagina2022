88230d3dd6d01bcc1be4dfc460baf8249862c825
Linerosky <daniellinerohd@gmail.com>
2022-09-22 20:06:13 -0500

    zaaaaaaaaaaaaz

--- historicos.php
+++ historicos.php
@@ -1,13 +1,13 @@
 <?php
 include 'varia.php';
-$Fechainicial = mysql_real_escape_string($_POST['Fechainicial']);
-$Fechafinal = mysql_real_escape_string($_POST['Fechafinal']);
-if ($Fechainicial =! null) {
-    $conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
-    $rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '$Fechainicial' AND '$Fechafinal'"); // genera el query a SQL
-    $fila = mysqli_fetch_all($rows, MYSQLI_ASSOC);
-    echo json_encode($fila);
-} else {
-    echo "ta mal";
-}
+$logFile = "view.log";
+$Fechainicial = $_POST['Fechainicial'];
+$Fechafinal = $_POST['Fechafinal'];
+file_put_contents($logFile, $Fechainicial, $Fechafinal);
+$conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
+$rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '14/09/2022' AND '23/09/2022'"); // genera el query a SQL
+$fila = mysqli_fetch_all($rows, MYSQLI_ASSOC);
+//echo json_encode($fila);
+echo $Fechainicial;
+echo $Fechafinal;
 ?>
