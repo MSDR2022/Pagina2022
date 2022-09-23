113ad43bb21d13a86a46ae5ac7b0b82425a11afe
Linerosky <daniellinerohd@gmail.com>
2022-09-22 19:57:51 -0500

    ta mal

--- historicos.php
+++ historicos.php
@@ -2,8 +2,12 @@
 include 'varia.php';
 $Fechainicial = mysql_real_escape_string($_POST['Fechainicial']);
 $Fechafinal = mysql_real_escape_string($_POST['Fechafinal']);
-$conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
-$rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '$Fechainicial' AND '$Fechafinal'"); // genera el query a SQL
-$fila = mysqli_fetch_all($rows, MYSQLI_ASSOC);
-echo json_encode($fila);
+if ($Fechainicial =! null) {
+    $conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
+    $rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '$Fechainicial' AND '$Fechafinal'"); // genera el query a SQL
+    $fila = mysqli_fetch_all($rows, MYSQLI_ASSOC);
+    echo json_encode($fila);
+} else {
+    echo "ta mal";
+}
 ?>
