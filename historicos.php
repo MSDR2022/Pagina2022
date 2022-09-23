55dee4b566435be2a5d84a824ac0d857b93d521c
Linerosky <daniellinerohd@gmail.com>
2022-09-22 19:35:25 -0500

    table();

--- historicos.php
+++ historicos.php
@@ -1,7 +1,7 @@
 <?php
 include 'varia.php';
-$_POST['Fechainicial'];
-$_POST['Fechafinal'];
+$Fechainicial = echo $_POST['Fechainicial'];
+$Fechafinal = echo $_POST['Fechafinal'];
 $conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
 $rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '$Fechainicial 00:00:00' AND '$Fechafinal 00:00:00'"); // genera el query a SQL
 $fila = mysqli_fetch_all($rows, MYSQLI_ASSOC);
