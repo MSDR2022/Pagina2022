cfbde54cfdebd3ddbb3380e67edc3893c1905dad
Linerosky <daniellinerohd@gmail.com>
2022-09-22 19:56:05 -0500

    mysql_real_escape_string

--- historicos.php
+++ historicos.php
@@ -1,7 +1,7 @@
 <?php
 include 'varia.php';
-$Fechainicial = echo $_POST['Fechainicial'];
-$Fechafinal = echo $_POST['Fechafinal'];
+$Fechainicial = mysql_real_escape_string($_POST['Fechainicial']);
+$Fechafinal = mysql_real_escape_string($_POST['Fechafinal']);
 $conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
 $rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '$Fechainicial' AND '$Fechafinal'"); // genera el query a SQL
 $fila = mysqli_fetch_all($rows, MYSQLI_ASSOC);
