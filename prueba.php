17988e8dd84f90a9b90a250e10f5a4c1a26f2e41
Diseng <mjguette21@gmail.com>
2022-09-24 17:30:54 -0500

    fetch 2.1

--- prueba.php
+++ prueba.php
@@ -1,9 +1,9 @@
 <?php
 $dat = ["success" => false];
 
-$_post = json_decode (file_get_contents('php://input'),true);
+$post1 = json_decode (file_get_contents('php://input'),true);
 if(isset($_post['texto']))
     $dat = ["success" => true];
 die(json_encode($dat));
-echo json_encode($_post);
+echo json_encode($post1);
 ?>
