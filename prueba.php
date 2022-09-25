ac1859ac99f3411a44eea8bd8e37abb35c367a8d
Diseng <mjguette21@gmail.com>
2022-09-24 21:07:27 -0500

    jquery

--- prueba.php
+++ prueba.php
@@ -1,9 +1,3 @@
 <?php
-$dat = ["success" => false];
-$post1 = json_decode (file_get_contents('php://input'),true);
-if(isset($_post['texto']))
-    $dat = ["success" => true];
-die(json_encode($dat));
-echo json_encode($post1);
-print_r($post1);
+$valor = $_POST['val'];
 ?>
