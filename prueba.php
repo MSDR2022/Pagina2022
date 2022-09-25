488a7b7acf28125df6ea4b0a93ddb48e01413b59
Diseng <mjguette21@gmail.com>
2022-09-24 20:42:17 -0500

    print_r

--- prueba.php
+++ prueba.php
@@ -3,6 +3,7 @@ $dat = ["success" => false];
 $post1 = json_decode (file_get_contents('php://input'),true);
 if(isset($_post['texto']))
     $dat = ["success" => true];
+    echo $post1;
 die(json_encode($dat));
-echo $post1;
+print_r($post1);
 ?>
