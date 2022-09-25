53e9b25122b43125beb1811e9e6bef6304b651aa
Diseng <mjguette21@gmail.com>
2022-09-25 13:37:46 -0500

    prueb

--- prueba.php
+++ prueba.php
@@ -1,4 +1,9 @@
 <?php
-$valor = $_POST['val'];
-echo($valor);
+include 'varia.php';
+$valori = $_POST['val1'];
+$valorf = $_POST['val2'];
+$conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
+$rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN $valori and $valorf"); // genera el query a SQL
+$fila = mysqli_fetch_all($rows, MYSQLI_ASSOC);
+echo json_encode($valor);
 ?>
