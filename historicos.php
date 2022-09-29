60aaceb75a21bf3b693ef74788c28fda09d9e5dc
Linerosky <daniellinerohd@gmail.com>
2022-09-29 14:59:59 -0500

    span css

--- historicos.php
+++ historicos.php
@@ -5,7 +5,7 @@ $fechafin = $_POST['fechafin'];
 $horaini = $_POST['horaini'];
 $horafin = $_POST['horafin'];
 $conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
-$rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '$fechaini' AND '$fechafin'"); // genera el query a SQL
+$rows = mysqli_query($conn, "SELECT * FROM Gps.db WHERE Fecha BETWEEN '$fechaini $horaini' AND '$fechafin $horafin'"); // genera el query a SQL
 $fila = mysqli_fetch_all($rows, MYSQLI_ASSOC);
 echo json_encode($fila);
 ?>
