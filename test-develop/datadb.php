39fadd43a070ccadc589166dbea7e0b88b6a0e1f
Linerosky <daniellinerohd@gmail.com>
2022-10-04 01:39:19 -0500

    test index

--- test-develop/datadb.php
+++ test-develop/datadb.php
@@ -1,7 +1,10 @@
 <?php
 include 'varia.php';
 $conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
-$rows = mysqli_query($conn, "SELECT * FROM Gps.db ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
+$rows = mysqli_query($conn, "SELECT * FROM Gps.dbtest WHERE ID = 1 ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
+$rows2 = mysqli_query($conn, "SELECT * FROM Gps.dbtest WHERE ID = 2 ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
 $fila = mysqli_fetch_assoc($rows);
+$fila2 = mysqli_fetch_assoc($rows2);
 echo json_encode($fila);
+echo json_encode($fila2);
 ?>
